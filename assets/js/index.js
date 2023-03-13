9f09008bc3999058827145852b9c444811a4c485
IgberZBN <igber_benitez@outlook.com>
2023-03-13 15:47:09 -0300

    Adicionando verifição do input

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 class QrCodeGenerator {
-  constructor(form, img, input) {
+  constructor(form, img, input, erroContent) {
     this.form = document.querySelector(form);
     this.inText = this.form.querySelector(input);
     this.img = document.querySelector(img);
@@ -14,6 +14,14 @@ class QrCodeGenerator {
 
   getValues() {
     const text = this.inText.value;
+
+    try {
+      this.checkValue(text);
+    } catch (e) {
+      erroGen.createErro(this.inText);
+      throw `${e}`;
+    }
+
     const url = this.getUrl(text);
     this.setSrc(url);
   }
@@ -27,6 +35,12 @@ class QrCodeGenerator {
     this.img.src = url;
   }
 
+  checkValue(text) {
+    if (!text) {
+      throw 'Insira um valor valido';
+    }
+  }
+
   init() {
     if (this.form) {
       this.addHandleClick();
@@ -35,5 +49,23 @@ class QrCodeGenerator {
   }
 }
 
+class ErroGenerator {
+  constructor(content) {
+    this.erroContent = document.querySelector(content);
+  }
+
+  createErro(inText) {
+    inText.classList.add('erro');
+    this.erroContent.classList.remove('d-n');
+
+    setTimeout(() => {
+      inText.classList.remove('erro');
+      this.erroContent.classList.add('d-n');
+    }, 3000);
+  }
+}
+
 const qrCodeGen = new QrCodeGenerator('.form', 'img', '.inText');
+
+const erroGen = new ErroGenerator('.erro-content');
 qrCodeGen.init();
